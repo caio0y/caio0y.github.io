2eadf2c09c046074d9929208c035f0f8637ff897
Caio Ribeiro <adv_caioribeiro@outlook.com>
2023-07-27 17:02:29 -0300

    update

--- script.js
+++ script.js
@@ -1,62 +1,61 @@
-// Obtenha uma referência ao elemento de input
+// Formatação do número de telefone do formulário
 const input = document.getElementById('whatsapp');
 
-// Adicione um evento de input para o input
 input.oninput = formatarTelefone;
 
 function formatarTelefone() {
-    // Obtenha o valor atual do input
-    let valor = input.value;
+  let valor = input.value;
 
-    // Remova todos os caracteres não numéricos
-    valor = valor.replace(/\D/g, '');
+  // Remova caracteres não numéricos
+  valor = valor.replace(/\D/g, '');
 
-    // Formate o número de telefone
-    if (valor.length > 11) {
-        // Se tiver mais de 11 dígitos, limite para 11 dígitos
-        valor = valor.slice(0, 11);
-    }
+  // limita a 11 digitos
+  if (valor.length > 11) {
+    valor = valor.slice(0, 11);
+  }
 
-    // Verifique o tamanho do número de telefone para aplicar a formatação correta
-    if (valor.length >= 1 && valor.length <= 2) {
-        valor = `(${valor}`;
-    } else if (valor.length > 2 && valor.length <= 7) {
-        valor = `(${valor.slice(0, 2)}) ${valor.slice(2)}`;
-    } else if (valor.length > 7 && valor.length <= 11) {
-        valor = `(${valor.slice(0, 2)}) ${valor.slice(2, 3)} ${valor.slice(3, 7)}-${valor.slice(7)}`;
-    }
+  // Aplica formatação
+  if (valor.length >= 1 && valor.length <= 2) {
+    valor = `${valor}`;
+  } else if (valor.length > 2 && valor.length <= 3) {
+    valor = `(${valor.slice(0, 2)}) ${valor.slice(2)}`;
+  } else if (valor.length > 3 && valor.length <= 7) {
+    valor = `(${valor.slice(0, 2)}) ${valor.slice(2, 3)} ${valor.slice(3, 7)}`;
+  } else if (valor.length > 7 && valor.length <= 11) {
+    valor = `(${valor.slice(0, 2)}) ${valor.slice(2, 3)} ${valor.slice(3, 7)}-${valor.slice(7)}`;
+  }
 
-    // Atualize o valor do input com a formatação
-    input.value = valor;
+  // Atualiza com a formatação
+  input.value = valor;
 }
 
-// Aguarde o DOM ser carregado
+// Envio de formulário por email
 document.addEventListener("DOMContentLoaded", function () {
-    // Obtenha o formulário pelo ID
-    const meuFormulario = document.getElementById("contatoForm");
 
-    meuFormulario.onsubmit = function (e) {
-      e.preventDefault(); // Impede o envio padrão do formulário
+  const formularioContato = document.getElementById("contatoForm");
 
-      // Obtenha os dados do formulário
-      const formData = new FormData(meuFormulario);
+  formularioContato.onsubmit = function (e) {
+    e.preventDefault(); // Impede que saia da página
 
-      // Envie os dados usando AJAX
-      const xhr = new XMLHttpRequest();
-      xhr.open("POST", meuFormulario.action, true);
-      xhr.setRequestHeader("Accept", "application/json");
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-          // Limpe o formulário após o envio bem-sucedido
-          meuFormulario.reset();
-          alert("Enviado com sucesso!"); // Opcional: mostre uma mensagem de sucesso
-        } else if (xhr.readyState === 4) {
-          alert("Ocorreu um erro."); // Opcional: mostre uma mensagem de erro
-        }
-      };
-      xhr.send(formData);
+    const formData = new FormData(formularioContato);
+
+    // Envio de dados usando AJAX
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", formularioContato.action, true);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        // mensagem de sucesso e reset do formulario
+        formularioContato.reset();
+        alert("Enviado com sucesso!");
+      } else if (xhr.readyState === 4) {
+        // mensagem de falha
+        alert("Ocorreu um erro.");
+      }
     };
-  });
+    xhr.send(formData);
+  };
+});
 
 
 
